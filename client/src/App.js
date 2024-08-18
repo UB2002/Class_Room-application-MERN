8576da20a3768e60579abe43d9439ec92ed59854
UB2002 <udaybhaskar0532@gmail.com>
2024-08-18 17:36:30 +0530

    add

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Login';
 import Dashboard from './Dashboard';
-import PrincipalDashboard from './PrincipalDashboard';
-import TeacherDashboard from './TeacherDashboard';
 
-function App() {
-    const role = localStorage.getItem('role');
-
+const App = () => {
     return (
-        <Router>
+        <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/" element={<Navigate to="/login" />} />
+                <Route path="/login" element={<Login />} />
                 <Route path="/dashboard" element={<Dashboard />} />
-                {/* Conditionally render routes based on role */}
-                {role === 'principal' && (
-                    <Route path="/principal-dashboard" element={<PrincipalDashboard />} />
-                )}
-                {role === 'teacher' && (
-                    <Route path="/teacher-dashboard" element={<TeacherDashboard />} />
-                )}
-                <Route path="*" element={<Navigate to="/" />} />
             </Routes>
-        </Router>
+        </BrowserRouter>
     );
-}
+};
 
 export default App;
